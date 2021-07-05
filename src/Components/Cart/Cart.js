d1b79812a4edce7bd945f2c2e1a6cd053fddfb6f
Kimchee <kchaase@gmail.com>
2021-07-04 20:15:52 -0600

    added state to manage cart visibility

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,14 +7,14 @@ import classes from './Cart.module.css'
 const Cart = props => {
     const cartItems = <ul className={classes['cart-items']}>{[{id:'c1', name:'Sushi',price:12.99}].map(item => <li>{item.name}</li>)}</ul>;
     return (
-        <Modal>
+        <Modal onCloseCart={props.onCloseCart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onCloseCart}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
